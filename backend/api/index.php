1a2d41ebe275c5332ef257c599cb609a998ce3bb
rkyarc <arickyh1@gmail.com>
2026-07-26 16:28:18 +0700

    chore: add debug-server endpoint

--- backend/api/index.php
+++ backend/api/index.php
@@ -46,6 +46,11 @@ try {
     $_SERVER['SCRIPT_NAME'] = '/index.php';
     $_SERVER['PHP_SELF'] = '/index.php';
 
+    if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], 'debug-server') !== false) {
+        echo json_encode($_SERVER);
+        exit;
+    }
+
     // Register the Composer autoloader...
     require __DIR__ . '/../vendor/autoload.php';
 
