4ff106d3722e41b5f0478e322c42a4bfe8da11ce
rkyarc <arickyh1@gmail.com>
2026-07-26 16:33:26 +0700

    fix: remove debug code and restore default exception handler

--- backend/api/index.php
+++ backend/api/index.php
@@ -81,7 +81,6 @@ try {
         'message' => $e->getMessage(),
         'file' => $e->getFile(),
         'line' => $e->getLine(),
-        'previous' => $e->getPrevious() ? $e->getPrevious()->getMessage() . ' in ' . $e->getPrevious()->getFile() . ':' . $e->getPrevious()->getLine() : null,
         'trace' => $e->getTraceAsString()
     ]);
     exit;
